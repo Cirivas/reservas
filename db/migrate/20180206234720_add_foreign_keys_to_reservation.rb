b6b04fded56b5354efaadbd01062dbf0d872c444
Camilo <crivas.folch@gmail.com>
2018-02-11 00:30:12 -0300

    fixed migration

--- db/migrate/20180206234720_add_foreign_keys_to_reservation.rb
+++ db/migrate/20180206234720_add_foreign_keys_to_reservation.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToReservation < ActiveRecord::Migration[5.1]
   def change
-    add_foreign_key :reservations, :user, column: :user_id
-    add_foreign_key :reservations, :user, column: :instructor_id
+    add_foreign_key :reservations, :users, column: :user_id
+    add_foreign_key :reservations, :users, column: :instructor_id
   end
 end

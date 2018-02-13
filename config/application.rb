9852aee9019a2e4062ea3cdf01ee96aad1f17ecc
Camilo <crivas.folch@gmail.com>
2018-02-13 20:52:17 -0300

    changed timezone; load qualifications at any given moment

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Reservas
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # America/Santiago zone
+    config.time_zone = 'Santiago'
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

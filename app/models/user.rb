a6b7b8bf68b8f13b8f6dcf7c0760b1cde37e38a7
Camilo <crivas.folch@gmail.com>
2018-04-17 19:57:06 -0300

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
   has_many :coflights, class_name: 'Reservation', foreign_key: 'instructor_id'
 
   validates :rut, presence: true, length: { minimum: 9, maximum: 10 }
-  validates :membership_number, presence: true, length: { is: 5 }
+  validates :membership_number, presence: true, length: { maximum: 5 }
   validates :email, presence: true, length: { maximum: 40 }
   validates :name, presence: true, length: { maximum: 25 }
   validates :last_name, presence: true, length: { maximum: 25 }
   validates :address, presence: true, length: { maximum: 50 }
-  validates :phone, presence: true, length: { is: 9 }
+  validates :phone, presence: true, length: { maximum: 9 }
   validates :country, presence: true, length: { maximum: 20 }
   validates :birthdate, presence: true
   validates :license_type, presence: true
-  validates :license_number, presence: true, length: { is: 9 }
+  validates :license_number, presence: true, length: { maximum: 9 }
   validates :user_role, presence: true
   validates :membership_type, presence: true
   validates :membership_state, presence: true

5cc2b86b536487159bf7e89316535d2720def4b2
Camilo <crivas.folch@gmail.com>
2018-05-08 16:57:14 -0300

    fixed edit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -67,7 +67,7 @@ class ReservationsController < ApplicationController
   end
 
   def edit
-    if user_signed_in? && (@reservation.user_id != current_user.id || !current_user.is_admin?)
+    if user_signed_in? && @reservation.user != current_user && !current_user.is_admin?
       redirect_to reservations_path
     end
   end
